8291fbe8eb94b7a83efe3329367784ce9bdd8cc2
BuildTools <unconfigured@null.spigotmc.org>
2019-07-01 14:25:19 -0700

    we made a input that take a name and prints something and, a conditional that detirmines f a number is greator then or less then 10.

---
+++ default.py
@@ -0,0 +1,19 @@
+def main():
+    print("Hello world")
+
+if __name__ == "__main__":
+    main()
+name = input("Whats your name?")
+print("%s is a very lovely name" % name)
+print(" ")
+print(" ")
+num = int(input("Enter a number"))
+
+if num > 10:
+    print("The number is greator then 10")
+elif num <10:
+    print("The number is less then 10")
+elif num == 10:
+    print("The number is 10")
+else:
+    print("You did not provide a valid number")
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello world")
-
-if __name__ == "main":
-    main()
-
